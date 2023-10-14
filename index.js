afe22faec56789fa9a57b9deaf0ebad321395088
Me <pdprince.lfc+github@gmail.com>
2023-10-14 15:32:47 -0400

    things

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value) {
+  if (value <= 199) {
+    return 'This one is on me!';
+  } else if (value <= 1500) {
+    return 'That will be twenty bucks.';
+  } else if (value <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  return cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous': {
+      return 'Thank you so much.';
+    }
+    case 'not as generous': {
+      return 'Thank you.';
+    }
+    default: {
+      return 'Bye.';
+    }
+  }
 }
